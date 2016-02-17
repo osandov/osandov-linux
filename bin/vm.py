e4df873ac67e5bdfcea4c20abe2dc56c87dc247a
Omar Sandoval <osandov@osandov.com>
2016-02-17 12:02:26 -0800

    vm.py: split into create and run subcommands
    The old `vm.py` is now `vm.py run` and there are no default options.
    Instead, `vm.py create` can be used to create a new VM, including
    creating the per-VM options and the root disk image.

--- bin/vm.py
+++ bin/vm.py
@@ -5,58 +5,115 @@ from collections import OrderedDict
 import os
 import os.path
 import pprint
+import shlex
 import subprocess
+import sys
 
 
 def main():
     parser = argparse.ArgumentParser(
         usage='usage: vm.py [OPTIONS] VM -- [QEMU_OPTION [QEMU_OPTION ...]]',
-        description='Run a virtual machine with QEMU over KVM')
-    parser.add_argument('vm', metavar='VM', help='name of the VM to run')
+        description='Manage QEMU virtual machines')
     parser.add_argument(
+        '--dry-run', action='store_true',
+        help='Print the command lines that would be run instead of running them')
+
+    subparsers = parser.add_subparsers(
+        title='command', description='command to run', dest='command')
+    subparsers.required = True
+
+    parser_create = subparsers.add_parser(
+        'create', help='create a new virtual machine')
+    parser_create.add_argument(
+        'name', metavar='NAME', help='name of the VM to create')
+    parser_create.add_argument(
+        '-c', '--cpu', type=str, default='1',
+        help='number of CPUs to give the guest (QEMU -smp option)')
+    parser_create.add_argument(
+        '-m', '--memory', type=str, default='1G',
+        help='amount of RAM to give the guest (QEMU -m option)')
+    parser_create.add_argument(
+        '-s', '--size', type=str, default=None,
+        help="size of the guest's root disk (can use k, M, G, and T suffixes)")
+    parser_create.set_defaults(func=cmd_create)
+
+    parser_run = subparsers.add_parser(
+        'run', help='run a virtual machine')
+    parser_run.add_argument(
+        'name', metavar='NAME', help='name of the VM to run')
+    parser_run.add_argument(
         'qemu_options', metavar='QEMU_OPTION', nargs='*',
         help='extra options to pass directly to QEMU')
-    parser.add_argument('-k', '--kernel', help='kernel in ~/linux/builds to run')
-    parser.add_argument(
+    parser_run.add_argument(
+        '-k', '--kernel', help='kernel in ~/linux/builds to run')
+    parser_run.add_argument(
         '-a', '--append', action='append', default=[],
-        help='append a command line argument for the kernel when passing -k')
+        help='append a kernel command line argument (only when passing -k)')
+    parser_run.set_defaults(func=cmd_run)
+
     args = parser.parse_args()
-
-    try:
-        run_vm(args)
-    except VMError as e:
-        exit(e)
+    args.func(args)
 
 
-class VMError(Exception):
-    pass
+def chdir(args, path, always_chdir=True):
+    if args.dry_run:
+        print('chdir {}'.format(shlex.quote(path)))
+    if (not args.dry_run) or always_chdir:
+        os.chdir(path)
 
 
-def run_vm(args):
-    os.chdir(os.path.expanduser('~/linux/vm'))
+def mkdir(args, path):
+    if args.dry_run:
+        print('mkdir {}'.format(shlex.quote(path)))
+    else:
+        os.mkdir(path)
 
-    if not os.path.isdir(args.vm):
-        raise VMError('No VM named {}'.format(args.vm))
 
-    # Default arguments.
-    qemu_options = [
-        ('-cpu', 'kvm64'),
-        ('-enable-kvm',),
-        ('-nographic',),
-        ('-m', '1G'),
-        ('-smp', '1'),
-        ('-watchdog', 'i6300esb'),
-        ('-drive', 'file={0}/{0}.qcow2,index=0,media=disk,if=virtio'.format(args.vm)),
-    ]
+def call(args, exec_args):
+    if args.dry_run:
+        print(' '.join(shlex.quote(arg) for arg in exec_args))
+    else:
+        subprocess.check_call(exec_args)
 
-    # Per-VM arguments.
-    try:
-        vm_script_path = '{}/vm.py'.format(args.vm)
-        with open(vm_script_path, 'r') as f:
-            code = compile(f.read(), vm_script_path, 'exec')
-            exec(code, globals(), locals())
-    except FileNotFoundError:
-        pass
+
+def my_input(prompt=None):
+    if prompt is not None:
+        sys.stderr.write(prompt)
+        sys.stderr.flush()
+    return input()
+
+
+def write_file(args, path, contents):
+    if args.dry_run:
+        print('cat << "EOF" > {}\n{}EOF'.format(shlex.quote(path), contents))
+    else:
+        with open(path, 'w') as f:
+            f.write(contents)
+
+def cmd_create(args):
+    chdir(args, os.path.expanduser('~/linux/vm'))
+    mkdir(args, args.name)
+    if args.size is None:
+        args.size = my_input('Size of root disk: ')
+    call(args, ['qemu-img', 'create', '-f', 'qcow2', '{0}/{0}.qcow2'.format(args.name), args.size])
+    write_file(args, '{}/vm.py'.format(args.name), """\
+add_option(qemu_options, '-cpu', 'kvm64')
+add_option(qemu_options, '-enable-kvm')
+add_option(qemu_options, '-smp', {cpu}),
+add_option(qemu_options, '-m', {memory}),
+add_option(qemu_options, '-watchdog', 'i6300esb'),
+add_option(qemu_options, '-drive', 'file={name}/{name}.qcow2,index=0,media=disk,if=virtio')
+""".format(name=args.name, cpu=args.cpu, memory=repr(args.memory)))
+
+
+def cmd_run(args):
+    chdir(args, os.path.expanduser('~/linux/vm'))
+
+    qemu_options = []
+    vm_script_path = '{}/vm.py'.format(args.name)
+    with open(vm_script_path, 'r') as f:
+        code = compile(f.read(), vm_script_path, 'exec')
+        exec(code, globals(), locals())
 
     # Command-line arguments.
     if args.kernel:
@@ -64,7 +121,7 @@ def run_vm(args):
         image_name = subprocess.check_output(['make', '-s', 'image_name'], cwd=build_path)
         image_name = image_name.decode('utf-8').strip()
         kernel_image_path = os.path.join(build_path, image_name)
-        add_option(qemu_options, '-kernel', kernel_image_path)
+        replace_option(qemu_options, '-kernel', kernel_image_path)
 
     explicit_append = False
 
@@ -87,27 +144,33 @@ def run_vm(args):
     for option in qemu_options:
         exec_args.extend(option)
 
-    returncode = subprocess.call(exec_args)
-    if returncode:
-        raise VMError('QEMU returned {}'.format(returncode))
-
-
-REPEATABLE_OPTIONS = {
-    '-drive'
-}
+    call(args, exec_args)
 
 
 def add_option(qemu_options, flag, *args):
+    # TODO: do the right thing (append or replace) for any flag
+    append_option(qemu_options, flag, *args)
+
+
+def append_option(qemu_options, flag, *args):
     assert isinstance(flag, str)
     for arg in args:
         assert isinstance(arg, str)
 
-    if flag not in REPEATABLE_OPTIONS:
-        pop_option(qemu_options, flag)
+    qemu_options.append((flag,) + args)
+
+
+def replace_option(qemu_options, flag, *args):
+    assert isinstance(flag, str)
+    for arg in args:
+        assert isinstance(arg, str)
+
+    pop_option(qemu_options, flag)
     qemu_options.append((flag,) + args)
 
 
 def append_to_cmdline(qemu_options, arg):
+    assert isinstance(arg, str)
     # TODO: quoting?
     old_options = pop_option(qemu_options, '-append')
     if old_options is None:
