de8d1ed8cc44cf9398df4a3dc642200592736faf
Omar Sandoval <osandov@osandov.com>
2016-07-05 16:37:49 -0700

    Add script to graft history.git to linux.git

---
+++ scripts/graft_linux_history.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+git remote add history https://git.kernel.org/pub/scm/linux/kernel/git/history/history.git
+git fetch -t history
+cat >> .git/info/grafts << EOF
+1da177e4c3f41524e886b7f1b8a0c1fc7321cac2 e7e173af42dbf37b1d946f9ee00219cb3b2bea6a
+7a2deb32924142696b8174cdf9b38cd72a11fc96 379a6be1eedb84ae0d476afbc4b4070383681178
+EOF
