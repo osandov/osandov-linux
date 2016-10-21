c921c235b45e1a93a6d1a07325e1c9f05778ccbf
Omar Sandoval <osandov@osandov.com>
2016-10-21 11:42:06 -0700

    burn_memory.py: use MAP_POPULATE instead of dirtying pages

--- scripts/burn_memory.py
+++ scripts/burn_memory.py
@@ -4,6 +4,9 @@ import argparse
 import mmap
 import os
 
+if not hasattr(mmap, 'MAP_POPULATE'):
+    mmap.MAP_POPULATE = 0x8000
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -15,19 +18,17 @@ def main():
     size = os.sysconf('SC_PHYS_PAGES')
     while size > 0:
         try:
-            mm = mmap.mmap(-1, size * page_size)
+            mm = mmap.mmap(-1, size * page_size,
+                           prot=mmap.PROT_READ | mmap.PROT_WRITE,
+                           flags=mmap.MAP_ANONYMOUS | mmap.MAP_PRIVATE | mmap.MAP_POPULATE)
             print('Allocated {} pages'.format(size))
-            # Dirty all of the pages so that they're actually allocated.
-            for i in range(size):
-                mm.seek(i * page_size)
-                mm.write_byte(0xff)
             # Keep a reference to the mmap object so that Python doesn't munmap
             # the memory.
             mms.append(mm)
         except OSError:
             print('Failed to allocate {} pages'.format(size))
             size //= 2
-    
+
 
 if __name__ == '__main__':
     main()
