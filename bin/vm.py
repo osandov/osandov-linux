d892a2106c423c0210174d83a3bb7a9c4b8f0a1b
Omar Sandoval <osandov@osandov.com>
2017-11-03 00:33:50 -0700

    vm.py: wait for network to come up in archinstall

--- bin/vm.py
+++ bin/vm.py
@@ -150,6 +150,10 @@ mirrors=({' '.join(shlex.quote(mirror) for mirror in args.pacman_mirrors)})
 packages=({' '.join(shlex.quote(package) for package in args.packages)})
 """)
     script.append(r"""
+while ! systemctl is-active -q network.target && ! systemctl is-failed -q network.target; do
+	sleep 1
+done
+
 export gateway="$(ip route show default | gawk 'match($0, /^\s*default.*via\s+([0-9.]+)/, a) { print a[1]; exit }')"
 [[ -z $gateway ]] && { echo "Could not find gateway" >&2; exit 1; }
 
