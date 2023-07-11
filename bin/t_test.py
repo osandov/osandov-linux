bf982de9de8ba39762f0e488afb2b85c6e039aa8
Omar Sandoval <osandov@osandov.com>
2023-07-11 15:07:32 -0700

    t_test.py: print statistics as part of progress

--- bin/t_test.py
+++ bin/t_test.py
@@ -138,6 +138,7 @@ commands differ significantly, we report the relation.
         metavar="COMMAND",
         help="shell command to run after each command",
     )
+
     parser.add_argument(
         "-v", "--verbose", action="store_true", help="print all samples"
     )
@@ -145,7 +146,7 @@ commands differ significantly, we report the relation.
         "--progress",
         choices=("auto", "always", "never"),
         default="auto",
-        help="display a progress bar",
+        help="display progress",
     )
     parser.add_argument(
         "--color",
@@ -153,6 +154,7 @@ commands differ significantly, we report the relation.
         default="auto",
         help="colorize results",
     )
+
     parser.add_argument(
         "commands", metavar="command1", action="append", help="first shell command"
     )
@@ -160,6 +162,25 @@ commands differ significantly, we report the relation.
         "commands", metavar="command2", action="append", help="second shell command"
     )
     args = parser.parse_args()
+
+    runs: Iterator[int]
+    if getattr(args, "order", "alternating") == "alternating":
+        runs = itertools.chain.from_iterable(
+            itertools.chain(
+                itertools.repeat(((0, False), (1, False)), args.warmup),
+                itertools.repeat(((0, True), (1, True)), args.repeat),
+            ),
+        )
+    else:  # args.order == "consecutive"
+        runs = itertools.chain(
+            itertools.repeat((0, False), args.warmup),
+            itertools.repeat((0, True), args.repeat),
+            itertools.repeat((1, False), args.warmup),
+            itertools.repeat((1, True), args.repeat),
+        )
+    num_runs = 2 * args.warmup + 2 * args.repeat
+
+    populations: List[Tuple[List[float], List[float]]] = []
 
     if args.color == "auto":
         color = sys.stdout.isatty()
@@ -179,50 +200,95 @@ commands differ significantly, we report the relation.
     else:
         red = green = bold = lambda s: s
 
-    runs: Iterator[int]
-    if getattr(args, "order", "alternating") == "alternating":
-        runs = itertools.chain.from_iterable(
-            itertools.chain(
-                itertools.repeat(((0, False), (1, False)), args.warmup),
-                itertools.repeat(((0, True), (1, True)), args.repeat),
-            ),
-        )
-    else:  # args.order == "consecutive"
-        runs = itertools.chain(
-            itertools.repeat((0, False), args.warmup),
-            itertools.repeat((0, True), args.repeat),
-            itertools.repeat((1, False), args.warmup),
-            itertools.repeat((1, True), args.repeat),
-        )
-    num_runs = 2 * args.warmup + 2 * args.repeat
-
     if args.progress == "auto":
-        progress = sys.stderr.isatty()
+        progress = sys.stdout.isatty()
     else:
         progress = args.progress == "always"
+
     if progress:
         num_runs_columns = len(str(num_runs))
         reserved_columns = 2 * num_runs_columns + 4
 
-        def print_progress_bar(i: int) -> None:
+    prev_num_progress_lines = 0
+
+    def print_progress(run: int) -> None:
+        lines = []
+
+        if progress:
             columns = shutil.get_terminal_size().columns
-            line = "\r"
             if columns > reserved_columns:
                 bar_columns = columns - reserved_columns
-                filled_columns = int(bar_columns * (i / num_runs))
+                filled_columns = int(bar_columns * (run / num_runs))
                 empty_columns = bar_columns - filled_columns
-                line += "[" + filled_columns * "#" + empty_columns * "-" + "] "
-            line += f"{i:>{num_runs_columns}}/{num_runs}"
-            sys.stderr.write(line)
+                progress_bar = "[" + filled_columns * "#" + empty_columns * "-" + "] "
+            else:
+                progress_bar = ""
+            progress_bar += f"{run:>{num_runs_columns}}/{num_runs}"
+            lines.append(progress_bar)
 
-    else:
+        for j, (samples1, samples2) in enumerate(populations, 1):
+            if j > 1:
+                lines.append("")
+            if len(populations) > 1:
+                lines.append(f"POPULATION {j}:")
+            means = [
+                numpy.mean(samples1) if samples1 else None,
+                numpy.mean(samples2) if samples2 else None,
+            ]
+            for i, samples in enumerate((samples1, samples2), 1):
+                if samples:
+                    lines.append(f"Command {i}:")
+                    lines.append(
+                        f"  n = {len(samples)} mean = {means[i - 1]:f} SD = {numpy.std(samples):f}"
+                    )
+                    lines.append(
+                        f"  min = {numpy.min(samples):f} max = {numpy.max(samples):f} median = {numpy.median(samples):f}"
+                    )
+                    if args.verbose:
+                        lines.append(
+                            "  samples = "
+                            + ", ".join([f"{sample:f}" for sample in samples])
+                        )
+            if samples1 and samples2:
+                lines.append(f"Difference of sample means = {means[0] - means[1]:f}")
+            if run == num_runs:
+                result = scipy.stats.ttest_ind(
+                    samples1, samples2, equal_var=args.equal_variances
+                )
+                lines.append(f"Test statistic = {result.statistic:f}")
+                if result.pvalue <= args.significance_level:
+                    rejected = bold("REJECTED") + ", "
+                    if means[0] < means[1]:
+                        rejected += green("command1 < command2")
+                    else:
+                        rejected += red("command1 > command2")
+                else:
+                    rejected = "FAILED TO REJECT"
+                lines.append(f"P(command1 = command2) = {result.pvalue:%} ({rejected})")
 
-        def print_progress_bar(i: int) -> None:
-            pass
+        if progress:
+            nonlocal prev_num_progress_lines
+            if prev_num_progress_lines:
+                if prev_num_progress_lines == 1:
+                    sys.stdout.write(f"\r\033[J")
+                elif prev_num_progress_lines > 0:
+                    sys.stdout.write(f"\r\033[{prev_num_progress_lines - 1}A\033[J")
+            prev_num_progress_lines = sum(
+                ((len(line) + columns - 1) // columns if line else 1) for line in lines
+            )
 
-    populations: List[Tuple[List[float], List[float]]] = []
+        sys.stdout.write("\n".join(lines))
+
+        if run == num_runs:
+            sys.stdout.write("\n")
+        else:
+            sys.stdout.flush()
+
+    if progress:
+        sys.stdout.write("\0337")
     for i, (command_index, record) in enumerate(runs):
-        print_progress_bar(i)
+        if progress:
+            print_progress(i)
 
         if args.pre is not None:
             subprocess.check_call(args.pre, shell=True)
@@ -240,44 +306,8 @@ commands differ significantly, we report the relation.
 
         if args.post is not None:
             subprocess.check_call(args.post, shell=True)
-    if progress:
-        sys.stderr.write("\r\033[K")
 
-    for j, (samples1, samples2) in enumerate(populations, 1):
-        if j > 1:
-            print()
-        if len(populations) > 1:
-            print(f"POPULATION {j}:")
-        means = numpy.mean(samples1), numpy.mean(samples2)
-        for i, samples in enumerate((samples1, samples2), 1):
-            print(f"Command {i}:")
-            print(
-                f"  n = {len(samples)} mean = {means[i - 1]:f} SD = {numpy.std(samples):f}"
-            )
-            print(
-                f"  min = {numpy.min(samples):f} max = {numpy.max(samples):f} median = {numpy.median(samples):f}"
-            )
-            if args.verbose:
-                print(
-                    "  samples =",
-                    ", ".join([f"{sample:f}" for sample in samples]),
-                )
-
-        result = scipy.stats.ttest_ind(
-            samples1, samples2, equal_var=args.equal_variances
-        )
-
-        print(f"Difference of sample means = {means[0] - means[1]:f}")
-        print(f"Test statistic = {result.statistic:f}")
-        if result.pvalue <= args.significance_level:
-            rejected = bold("REJECTED") + ", "
-            if means[0] < means[1]:
-                rejected += green("command1 < command2")
-            else:
-                rejected += red("command1 > command2")
-        else:
-            rejected = "FAILED TO REJECT"
-        print(f"P(command1 = command2) = {result.pvalue:%} ({rejected})")
+    print_progress(num_runs)
 
 
 if __name__ == "__main__":
